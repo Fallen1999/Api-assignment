f0981c46eec6b2e49b25a6be0fb0f3ee42472c59
cb007993 <cb007993@students.apiit.lk>
2020-03-13 11:47:51 +0530

    final commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, redirect, render_template, jsonify
+from pymongo import MongoClient
+from bson import json_util
+from setuptools import setup
+import requests
+
+
+app = Flask(__name__)
+
+client = MongoClient()
+db = client["APIIT"]
+collection = db.to_dos
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search')
+def search():
+
+    recipe = request.args.get('a', 0, type=str)
+
+    response = requests.get(f"https://www.amiiboapi.com/api/amiibo/?name={recipe}")
+
+    response = response.json()["amiibo"][0]
+    print(response)
+    return jsonify(result=response)
+
+
+
+@app.route('/view', methods=['GET'])
+def get_todos():
+    to_dos = list(collection.find())
+    return json_util.dumps(to_dos)
+
+
+
+
+@app.route('/add', methods=['POST'])
+def add_todo():
+    if request.method == 'POST':
+        new_todo = request.get_json()
+        name = new_todo['name']
+        description = new_todo['description']
+        time = new_todo['time']
+
+        collection.insert_one({
+            "name": name, 
+            "description": description,
+            "time": time
+        })
+        return redirect('/view')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8000', debug=True)
